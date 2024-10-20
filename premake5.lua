3ecb63dcb85a8afc62adda90b19d9249c2a1649e
Fynotix <dollorrblx@gmail.com>
2024-10-20 16:10:18 -0600

    add premake5

---
+++ premake5.lua
@@ -0,0 +1,41 @@
+project "ImGui"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "off"
+
+    targetdir("bin/"..outputdir.."/%{prj.name}")
+    objdir("bin/"..outputdir.."/%{prj.name}/int")
+
+    files {
+        "imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_tables.cpp",
+        "imgui_widgets.cpp",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+        "imgui_demo.cpp",
+
+        "backends/imgui_impl_sdlrenderer2.h",
+        "backends/imgui_impl_sdlrenderer2.cpp"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+
+    filter "system:linux"
+        pic "On"
+        systemversion "latest"
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
+        symbols "off"
